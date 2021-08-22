684260432c4206a6f954ddf997f67ce7e89652e5
johnabass <johnbass1017@yahoo.com>
2021-08-21 17:07:07 -0700

    allow for restarting a Switch by ensuring that loop cleans up resources properly

--- switch.go
+++ switch.go
@@ -11,12 +11,23 @@ import (
 )
 
 const (
-	DefaultTTL    time.Duration = 1 * time.Minute
-	DefaultMisses               = 0
+	// DefaultSource is the postpone source used when no source is supplied
+	DefaultSource = "<unset>"
+
+	// DefaultTTL is the time-to-live for switches when no TTL is supplied or
+	// when the TTL is nonpositive.
+	DefaultTTL time.Duration = 1 * time.Minute
+
+	// DefaultMisses is the number of allowed missed postpones before triggering
+	// actions when the misses are not supplied or are nonpositive.
+	DefaultMisses = 0
 )
 
 var (
+	// ErrSwitchStarted is returned by Switch.Start if a Switch is currently running.
 	ErrSwitchStarted = errors.New("That switch has already been started")
+
+	// ErrSwitchStopped is returned by Switch.Stop if a Switch is not running.
 	ErrSwitchStopped = errors.New("That swith has not been started")
 )
 
@@ -37,6 +48,8 @@ type PostponeRequest struct {
 	RemoteAddr string
 }
 
+// String returns a human-readable representation of this request.  This is the string
+// output to stdout each time a postpone request is received.
 func (pr PostponeRequest) String() string {
 	source := pr.Source
 	if len(source) == 0 {
@@ -50,10 +63,13 @@ func (pr PostponeRequest) String() string {
 	}
 }
 
+// Postponer represents something that can postpone triggering actions.
 type Postponer interface {
 	Postpone(PostponeRequest) bool
 }
 
+// Switch is a dead man's switch.  This type is associated with a slice of Actions which
+// will be executed unless postponed within a certain time-to-live interval.
 type Switch struct {
 	logger Logger
 
@@ -68,6 +84,8 @@ type Switch struct {
 	cancel    chan struct{}
 }
 
+// NewSwitch constructs a Switch.  If actions is empty, the returned Switch won't do
+// anything when triggered.
 func NewSwitch(l Logger, ttl time.Duration, maxMisses int, actions ...Action) *Switch {
 	if ttl <= 0 {
 		ttl = DefaultTTL
@@ -86,7 +104,25 @@ func NewSwitch(l Logger, ttl time.Duration, maxMisses int, actions ...Action) *S
 	}
 }
 
+// cleanup handles shutdown of this switch's concurrent resources.  If the switch
+// was actually stopped, this method returns true.  If the switch wasn't running,
+// this method returns false.
+func (s *Switch) cleanup() (stopped bool) {
+	s.stateLock.Lock()
+	defer s.stateLock.Unlock()
+
+	stopped = s.cancel != nil
+	if stopped {
+		close(s.cancel)
+	}
+
+	s.postpone = nil
+	s.cancel = nil
+	return
+}
+
 func (s *Switch) loop(postpone <-chan PostponeRequest, cancel <-chan struct{}) {
+	defer s.cleanup()
 	var misses int
 
 	for {
@@ -134,6 +170,11 @@ func (s *Switch) Postpone(u PostponeRequest) (postponed bool) {
 	return
 }
 
+// Start begins waiting for postpone requests.  If no request is received in the time-to-live
+// interval, the actions will trigger.
+//
+// If the actions trigger, or if Stop is called, this method may be called again.  If this
+// switch has already been started but has not triggered its actions yet, ErrSwitchStarted is returned.
 func (s *Switch) Start(context.Context) error {
 	s.stateLock.Lock()
 	defer s.stateLock.Unlock()
@@ -148,18 +189,18 @@ func (s *Switch) Start(context.Context) error {
 	return nil
 }
 
-func (s *Switch) Stop(context.Context) error {
-	s.stateLock.Lock()
-	defer s.stateLock.Unlock()
-
-	if s.cancel == nil {
-		return ErrSwitchStopped
+// Stop discontinues waiting for postpone requests.  The actions will not be triggered, unless
+// they were already triggered by missed postpone requests.
+//
+// If this switch wasn't running or had already triggered actions, this method returns ErrSwitchStopped.
+//
+// If this switch was running and had not triggered, this method returns nil.
+func (s *Switch) Stop(context.Context) (err error) {
+	if !s.cleanup() {
+		err = ErrSwitchStopped
 	}
 
-	close(s.cancel)
-	s.postpone = nil
-	s.cancel = nil
-	return nil
+	return
 }
 
 func provideSwitch() fx.Option {
