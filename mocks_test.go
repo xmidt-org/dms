730d87eafe3c5320bd318917a1eb5ea3f8de405c
johnabass <johnbass1017@yahoo.com>
2021-09-16 08:46:47 -1000

    pruned switch tests for another branch

--- mocks_test.go
+++ mocks_test.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"go.uber.org/fx"
 )
+
+type testAction struct {
+	t *testing.T
+
+	label  string
+	err    error
+	called bool
+}
+
+func (ta testAction) String() string {
+	return ta.label
+}
+
+func (ta testAction) Run() error {
+	assert.Falsef(ta.t, ta.called, "Action %s has already been called", ta.label)
+	return ta.err
+}
 
 type mockShutdowner struct {
 	mock.Mock
